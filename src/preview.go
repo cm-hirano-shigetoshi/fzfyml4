98ca1368545e96f4ae5e12738d390e5671c07097
hirano.shigetoshi <hirano.shigetoshi@classmethod.jp>
2021-07-15 22:11:39 +0900

    無用なpreviewを展開しない

--- src/preview.go
+++ src/preview.go
@@ -1,7 +1,8 @@
 package fzfyml
 
 import (
-//"fmt"
+	//"fmt"
+	"strings"
 )
 
 type Preview struct {
@@ -10,15 +11,30 @@ type Preview struct {
 }
 
 func (preview *Preview) init(p map[string]interface{}) {
-	preview.command = p["command"].(string)
-	preview.window = p["window"].(string)
+	if command, ok := p["command"].(string); ok {
+		preview.command = command
+	}
+	if window, ok := p["window"].(string); ok {
+		preview.window = window
+	}
 }
 
 func (preview *Preview) getPreviewText(exe string, textFilePath string, indexFilePath string, delimiter interface{}) string {
-	if indexFilePath == "" {
-		return "--preview '" + preview.command + "' --preview-window '" + preview.window + "'"
-	} else {
-		expanded_command := expandFieldIndex(preview.command, exe, textFilePath, delimiter)
-		return "--preview '" + "echo {+n} > " + indexFilePath + "; " + expanded_command + "' --preview-window '" + preview.window + "'"
+	options := []string{}
+	if len(preview.command) > 0 {
+		options = append(options, "--preview")
+		command := ""
+		if indexFilePath == "" {
+			command = "'" + preview.command + "'"
+		} else {
+			expanded_command := expandFieldIndex(preview.command, exe, textFilePath, delimiter)
+			command = "'" + "echo {+n} > " + indexFilePath + "; " + expanded_command + "'"
+		}
+		options = append(options, command)
 	}
+	if len(preview.window) > 0 {
+		options = append(options, "--preview-window")
+		options = append(options, preview.window)
+	}
+	return strings.Join(options, " ")
 }
